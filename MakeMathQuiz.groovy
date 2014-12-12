4bb9e508177d38208ef458b4c2dcc5a9b94ce36c
Dan Tanner <dan@dantanner.com>
2014-12-11 22:01:52 -0600

    implemented division option more better

--- MakeMathQuiz.groovy
+++ MakeMathQuiz.groovy
@@ -9,8 +9,8 @@ class MakeMathQuiz {
     def number_of_rows = 8
     def number_of_columns = 5
     def highest_number_to_use_in_test = 5
-    def number_of_tests_to_create = 2
-    def type_of_test = "subtract" // one of add, subtract, multiply, divide
+    def number_of_tests_to_create = 5
+    def type_of_test = "divide" // one of add, subtract, multiply, divide
 
     // you don't need to change anything below this
 
@@ -25,15 +25,28 @@ class MakeMathQuiz {
         test.createHtmlFile("math-test.html")
     }
 
-
     int getRandomNumberBetween(int minValue, int maxValue) {
         random.nextInt(maxValue) + minValue
+    }
+
+    List getPrimeFactors(int target) {
+        if (target == 1) return [1]
+     
+        if (target < 4) return [1, target]
+     
+        def targetSqrt = Math.sqrt(target)
+        def lowfactors = (2..targetSqrt).grep { (target % it) == 0 }
+        if (lowfactors == []) return [1, target]
+        def nhalf = lowfactors.size() - ((lowfactors[-1] == targetSqrt) ? 1 : 0)
+     
+        [1] + lowfactors + (0..<nhalf).collect { target.intdiv(lowfactors[it]) }.reverse() + [target]
     }
 
     String buildCell() {
         if (type_of_test == "add") return buildAdditionCell()
         if (type_of_test == "subtract") return buildSubtractionCell()
         if (type_of_test == "multiply") return buildMultiplicationCell()
+        if (type_of_test == "divide") return buildDivisionCell()
         throw new RuntimeException("I don't know what type of cell to build.")
     }
 
@@ -48,6 +61,23 @@ class MakeMathQuiz {
         int topValue = getRandomNumberBetween(2, highest_number_to_use_in_test)
         """${topValue} <br>
            - ${getRandomNumberBetween(1, topValue - 1)} <br>
+          <hr width=40px>
+        """
+    }
+
+    String buildDivisionCell() {
+        int numerator = (getRandomNumber(highest_number_to_use_in_test) + 1) * (getRandomNumber(highest_number_to_use_in_test) + 1)
+        List factors = getPrimeFactors(numerator)
+        // if we have more than 1 and the number, remove them to ensure a more interesting division problem
+        if (factors.size() > 2) {
+            factors.remove(factors.size() - 1)
+            factors.remove(0)
+        }
+        Collections.shuffle(factors, new Random())
+        int denominator = factors[0]
+        
+        """${numerator} <br>
+           &divide; ${denominator} <br>
           <hr width=40px>
         """
     }
